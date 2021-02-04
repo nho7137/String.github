dd4330a39686cf0474a2f8671c70b6fa48220a75
student <student@kvcc.dom>
2021-02-04 13:47:50 -0500

    Stringweek3

---
+++ Strings.Git.py
@@ -0,0 +1,10 @@
+def compare_string(string1, string2):
+    string1 = string1.lower()
+    string2 = string2.lower()
+    string1 = string1.strip()
+    string2 = string2.strip()
+    if string1 == string2: 
+        return True 
+    else: 
+        return False 
+    
