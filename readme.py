5bf24fb064831116e34f2ba721cf25437e86742a
Nguyen Ho <78519556+nho7137@users.noreply.github.com>
2021-02-09 01:42:09 -0500

    file3
    Hello World!

---
+++ readme.py
@@ -0,0 +1,13 @@
+# CIS 216 Python Programming # 12292
+# Instructor: Steven H Johnson 
+# Student: Nguyen Ho 
+# Date: Feb-9-2021
+
+# Homework Assignments 
+
+1. Hello, world! &mdash; Basic assignment to practice using Git and [Pytest](http://docs.pytest.org/en/estable/getting.started.html).
+
+# Things you have learned 
+Add to this readme anything you have learned
+
+* [first item learned]
